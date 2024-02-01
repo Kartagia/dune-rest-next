1e7767f85a0da09bb65f6c3186d9c4e41dd111e3
Antti Kautiainen <antti@kautiainen.com>
2024-02-01 09:16:11 +0200

    test: isTrait test.
    modified:   test/character.test.mjs
    - Testing isTrait gatekeeper function.

--- test/character.test.mjs
+++ test/character.test.mjs
@@ -3,6 +3,7 @@ import { describe, it } from "mocha";
 import { expect } from "chai";
 import { AssertionError } from "assert";
 import { isInteger, toInteger, assertInteger } from "../src/model/character.mjs";
+import { isTrait } from "../src/model/character.mjs";
 
 describe("Testing Integer", () => {
     const validIntegers = [Number.MIN_SAFE_INTEGER, -(2**32), -(2**32)-1, -256, -255, -1, "-1", 0, "0", 1, "1", 255, 256, (2**32), (2**32)-1, Number.MAX_SAFE_INTEGER];
@@ -48,4 +49,22 @@ describe("Testing Integer", () => {
         })
 
     })
+})
+
+describe("Trait", () => {
+    const validTraits = [
+        {isTrait: true, name: "Thief", description: "A thief skilled in the arts of burglary and pickpocketting"}, 
+        {isTrait: true, name: "Bene Gesserit trained", 
+        description: "A layman member of the order with the Bene Gesserit training for noble daughters"}
+    ];
+    const invalidTraits = [undefined, null, "Thief", { ...validTraits[0], name: undefined}, {isTrait: true }];
+    
+    it("isTrait", () => {
+        validTraits.forEach( (tested, index) => {
+            expect(isTrait(tested, `Expected a valid trait #${index} to be true`)).true;
+        })
+        invalidTraits.forEach( (tested, index) => {
+            expect(isTrait(tested, `Expected an invalid trait #${index} to be false`)).false;
+        })
+    });
 })
